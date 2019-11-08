39bd26bfc9fb5c40eb6ec716fc9bb3c7af4a1f7c
Elizabeth Villalejos <nekobanpaia@gmail.com>
2019-11-07 17:51:15 -0700

    updating test legibility of calc

--- main/calculator/test.js
+++ main/calculator/test.js
@@ -4,23 +4,23 @@ test('calculator function exists', () => {
   expect(calculator).toBeDefined();
 });
 
-test('add 2+2', () => {
+test('add 2 + 2', () => {
   expect(calculator.add(2, 2)).toBe(4);
 });
 
-test('add 2+(-3)', () => {
+test('add 2 + (-3)', () => {
   expect(calculator.add(2, -3)).toBe(-1);
 });
 
-test('substract 2-2', () => {
+test('substract 2 - 2', () => {
   expect(calculator.subtract(2, 2)).toBe(0);
 });
 
-test('substract 2-(-2)', () => {
+test('substract 2 - (-2)', () => {
   expect(calculator.subtract(2, -2)).toBe(4);
 });
 
-test('multiply 2*3', () => {
+test('multiply 2 * 3', () => {
   expect(calculator.multiply(2, 3)).toBe(6);
 });
 
